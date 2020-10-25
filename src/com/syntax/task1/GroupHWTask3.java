e723cdc5c77a9094a0f8352b3694a8ed715f74dd
KurbakoAnastasiia <kurbako.anastasia@gmail.com>
2020-10-25 09:17:11 -0400

    Group Project 1

---
+++ src/com/syntax/task1/GroupHWTask3.java
@@ -0,0 +1,23 @@
+package com.syntax.task1;
+
+
+public class GroupHWTask3 {
+
+	public static void main(String[] args) {
+		
+ 
+		int num1, num2, num3;
+	 num1 = 0;
+	 num2 = 1; 
+		System.out.print(num1 + " " + num2 + " ");
+	 for (int i = 0; i < 8; i++) {
+			num3 = num1 + num2;
+			System.out.print(num3 + " ");
+			num1 = num2;
+			num2 = num3;
+			
+		}
+
+	}
+
+}
